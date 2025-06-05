d7716e25e0bef9c22f84cc7a758e7fe44e7d6e36
NavodContrath <navod.contrath@gmail.com>
2025-06-05 13:34:04 +0200

    Add snack-5.js: implement logEverySecond function to print a message every second

---
+++ snack-5.js
@@ -0,0 +1,27 @@
+/* 
+Snack 5
+Crea una funzione stampaOgniSecondo con setInterval.
+Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.*/
+
+setTimeout(() => {
+    console.log("fifth snack")
+
+    function logEverySecond(ms) {
+        let counter = 0
+        const interval = setInterval(() => {
+            console.log(`sono passati ${counter} secondi`)
+            counter++
+            if (counter > 10) {
+                clearInterval(interval)
+            }
+        }, ms)
+
+
+    }
+
+    logEverySecond(1000)
+}, 3000)
+
+//usato un setTimeout per non far accavallare i log in console

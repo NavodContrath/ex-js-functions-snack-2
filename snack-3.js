66bf6127b9b058292dbd8b9a1b3dd549ebfae660
NavodContrath <navod.contrath@gmail.com>
2025-06-05 12:55:28 +0200

    Refactor snack-3.js: update function definition and improve comments for clarity

--- snack-3.js
+++ snack-3.js
@@ -1,6 +1,14 @@
 /* 
 Snack 3
 Crea una funzione eseguiOperazione:
-Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri. */
+Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback).
+La funzione deve eseguire l'operazione fornita sui due numeri. */
 
 console.log("third snack")
+
+const sum4 = (numb1, numb2) => numb1 + numb2
+const multiply = (numb1, numb2) => numb1 * numb2
+const executeOperation = (numb1, numb2, operation) => operation(numb1, numb2)
+
+console.log(executeOperation(2, 3, sum4))
+console.log(executeOperation(2, 3, multiply))
